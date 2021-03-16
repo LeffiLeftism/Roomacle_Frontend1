4182ee9557d2d016c11ab4b9b220dfe80b03a121
LeffiLeftism <35105361+LeffiLeftism@users.noreply.github.com>
2021-03-16 17:15:27 +0100

    Create Post Comment

---
+++ src/components/Evpost.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="evpost">
+    <span class="textline">{{ num }} | {{ name }}</span
+    ><br />
+    {{ tStart }} - {{ tEnd }}
+    <br />
+    {{ dozent }}
+    <br/>
+    <span v-for="(item, index)  in members" v-bind:key ="index">
+        {{ item }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    num: Number,
+    name: String,
+    tStart: String,
+    tEnd: String,
+    dozent: String,
+    members: Array,
+  },
+};
+</script>
+
+<style>
+#evpost {
+  padding: 7px;
+  text-align: left;
+  background-color: rgb(75, 204, 243);
+}
+.textline {
+  font-size: 1em;
+  font-weight: bold;
+}
+</style>
