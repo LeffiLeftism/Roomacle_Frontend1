63e298fd8b0b4a5fa5c8fdd422d4aa0fdce6dcd7
LeffiLeftism <35105361+LeffiLeftism@users.noreply.github.com>
2021-03-18 01:37:26 +0100

    Add Vue-Modal-2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
 
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './plugins/bootstrap-vue'
+
+import Modal from "@burhanahmeed/vue-modal-2";
+
+Vue.use(Modal);
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
